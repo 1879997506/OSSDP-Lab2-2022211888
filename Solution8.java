a2208514ece2b841f51297195ed41efb0f534172
zzy <1879997506@qq.com>
2024-10-18 21:06:07 +0800

    第八题修正“

--- Solution8.java
+++ Solution8.java
@@ -1,3 +1,5 @@
+package org.example;
+
 /**
  * @description:
  *
@@ -37,15 +39,15 @@
  * grid[i][j] 的值为 '0' 或 '1'
  *
  */
-class Solution8 {
+public class Solution8 {
     void dfs(char[][] grid, int r, int c) {
         int nr = grid.length;
         int nc = grid[0].length;
 
-        if (r < 0 || c > 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
             return;
         }
-        grid[r][c] = '1';
+        grid[r][c] = '0';
         dfs(grid, r - 1, c);
         dfs(grid, r + 1, c);
         dfs(grid, r, c - 1);
@@ -53,7 +55,7 @@ class Solution8 {
     }
 
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length <= 1) {
+        if (grid == null || grid.length ==0 || grid[0].length ==0) {
             return 0;
         }
 
@@ -61,7 +63,7 @@ class Solution8 {
         int nc = grid[0].length;
         int num_islands = 0;
         for (int r = 0; r < nr; ++r) {
-            for (int c = 0; r < nc; ++c) {
+            for (int c = 0; c < nc; ++c) {
                 if (grid[r][c] == '1') {
                     ++num_islands;
                     dfs(grid, r, c);
